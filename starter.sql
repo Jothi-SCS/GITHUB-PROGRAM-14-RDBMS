5a4b495d705bf0003e503df92be1aad2b4b85416
Jothi-SCS <54353240+Jothi-SCS@users.noreply.github.com>
2026-09-11 11:24:16 +0530

    Create starter.sql

---
+++ starter.sql
@@ -0,0 +1,21 @@
+-- Program: Sum of Two Numbers using PL/SQL
+-- Name:
+-- Reg No:
+
+SET SERVEROUTPUT ON;
+
+DECLARE
+    num1 NUMBER := 10;
+    num2 NUMBER := 20;
+
+    -- Declare variable here
+    total NUMBER;
+
+BEGIN
+
+    -- Write code here
+
+    DBMS_OUTPUT.PUT_LINE('Sum = ' || total);
+
+END;
+/
